f99d8608fb3f35cb5bacc9cde6f8ce1e24016b3a
Serhii <sirozhka.v@gmail.com>
2023-12-20 10:45:52 +0100

    Chakra UI dark mode setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,23 @@ import App from './App.jsx';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode('gray.100', '#000')(props),
+      color: mode('gray.800', 'whiteAlpha.900')(props),
+    },
+  }),
+};
 
 const config = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({ config, styles });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
