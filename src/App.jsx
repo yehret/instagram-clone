e230d1a3bef873dca4c6a4c590c58b79a647a999
Serhii <sirozhka.v@gmail.com>
2023-12-20 11:46:14 +0100

    homepage&sidebar base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
+import PageLayout from './Layouts/PageLayout';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<AuthPage />} />
-      </Routes>
+      <PageLayout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </PageLayout>
     </>
   );
 }
---
+++ src/Layouts/PageLayout/index.jsx
@@ -0,0 +1,25 @@
+import { Box, Flex } from '@chakra-ui/react';
+import Sidebar from '../../components/Sidebar';
+import { useLocation } from 'react-router-dom';
+
+const PageLayout = ({ children }) => {
+  const { pathname } = useLocation();
+
+  return (
+    <Flex>
+      {/* left sidebar */}
+      {pathname !== '/auth' ? (
+        <Box w={{ base: '70px', md: '240px' }}>
+          <Sidebar />
+        </Box>
+      ) : null}
+
+      {/* the whole content on the right */}
+      <Box flex={1} w={{ base: 'calc(100% - 70px)', md: 'calc(100% - 240px)' }}>
+        {children}
+      </Box>
+    </Flex>
+  );
+};
+
+export default PageLayout;
