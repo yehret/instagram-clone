36800ea01a9e142fb6261e5115dd7ae7d8a1f879
Serhii <sirozhka.v@gmail.com>
2023-12-20 14:16:03 +0100

    post footer layout

--- src/components/FeedPosts/FeedPost.jsx
+++ src/components/FeedPosts/FeedPost.jsx
@@ -6,7 +6,7 @@ const FeedPost = () => {
   return (
     <>
       <PostHeader />
-      <Box>
+      <Box my={2}>
         <Image src="/img1.png" alt="user profile pic" />
       </Box>
       <PostFooter />
