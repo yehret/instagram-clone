a9a2cc24137bfd18ce10cd99edb3295f9c3564f4
Serhii <sirozhka.v@gmail.com>
2023-12-21 17:05:18 +0100

    user log out minor fixes

--- src/hooks/useSignUpWithEmailAndPassword.js
+++ src/hooks/useSignUpWithEmailAndPassword.js
@@ -2,11 +2,13 @@ import { useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth'
 import {auth, firestore} from '../firebase/firebase'
 import { doc, setDoc } from 'firebase/firestore'
 import useShowToast from './useShowToast'
+import useAuthStore from '../store/authStore'
 
 const useSignUpWithEmailAndPassword = () => {
 
    const [createUserWIthEmailAndPassword, ,loading, error] = useCreateUserWithEmailAndPassword(auth)
    const showToast = useShowToast()
+   const loginUser = useAuthStore(state => state.login)
 
    const signup = async (inputs) => {
       if(!inputs.email || !inputs.password || !inputs.username || !inputs.fullName) {
@@ -35,6 +37,7 @@ const useSignUpWithEmailAndPassword = () => {
 
             await setDoc(doc(firestore, "users", newUser.user.uid), userDoc)
             localStorage.setItem("user-info", JSON.stringify(userDoc))
+            loginUser(userDoc)
          }
       } catch (error) {
          showToast("Error", error.message, "error")
