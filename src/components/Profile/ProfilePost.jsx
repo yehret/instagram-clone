44d7e467edb74626bcccb7f4fa5cd2f997a71430
Serhii <sirozhka.v@gmail.com>
2023-12-23 13:14:09 +0100

    delete post functionality

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,6 +1,5 @@
 import {
   Avatar,
-  Box,
   Button,
   Divider,
   Flex,
@@ -23,11 +22,43 @@ import Comment from '../Comment/Comment';
 import PostFooter from '../FeedPosts/PostFooter';
 import useUserProfileStore from '../../store/userProfileStore';
 import useAuthStore from '../../store/authStore';
+import useShowToast from '../../hooks/useShowToast';
+import { useState } from 'react';
+import { deleteObject, ref } from 'firebase/storage';
+import { firestore, storage } from '../../firebase/firebase';
+import { arrayRemove, deleteDoc, doc, updateDoc } from 'firebase/firestore';
+import usePostStore from '../../store/postStore';
 
 const ProfilePost = ({ post }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const userProfile = useUserProfileStore((state) => state.userProfile);
   const authUser = useAuthStore((state) => state.user);
+  const showToast = useShowToast();
+  const [isDeleting, setIsDeleting] = useState(false);
+  const deletePost = usePostStore((state) => state.deletePost);
+  const deletePostFromProfile = useUserProfileStore((state) => state.deletePost);
+
+  const handleDeletePost = async () => {
+    if (!window.confirm('Are you sure you want to delete this post?')) return;
+    if (isDeleting) return;
+    try {
+      const imageRef = ref(storage, `posts/${post.id}`);
+      await deleteObject(imageRef);
+
+      const userRef = doc(firestore, 'users', authUser.uid);
+      await deleteDoc(doc(firestore, 'posts', post.id));
+
+      await updateDoc(userRef, { posts: arrayRemove(post.id) });
+
+      deletePost(post.id);
+      deletePostFromProfile(post.id);
+      showToast('Success', 'Post deleted successfully', 'success');
+    } catch (error) {
+      showToast('Error', error.message, 'error');
+    } finally {
+      setIsDeleting(false);
+    }
+  };
 
   return (
     <>
@@ -108,6 +139,8 @@ const ProfilePost = ({ post }) => {
                   </Flex>
                   {authUser?.uid === userProfile.uid && (
                     <Button
+                      onClick={handleDeletePost}
+                      isLoading={isDeleting}
                       _hover={{ bg: 'whiteAlpha.300', color: 'red.600' }}
                       borderRadius={4}
                       p={1}
--- src/store/userProfileStore.js
+++ src/store/userProfileStore.js
@@ -5,6 +5,12 @@ const useUserProfileStore = create((set) => ({
    setUserProfile: (userProfile) => set({userProfile}),
    addPost:(post) => set(state => ({
       userProfile: {...state.userProfile, posts:[post.id, ...state.userProfile.posts]}
+   })),
+   deletePost: (postId) => set(state => ({
+      userProfile: {
+         ...state.userProfile,
+         posts: state.userProfile.posts.filter((id) => id !== postId)
+      }
    }))
 }))
 
