cc52616e2a0619f9c0a76b694d6072e58987b80f
Serhii <sirozhka.v@gmail.com>
2023-12-23 11:41:12 +0100

    search state minor fix

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -12,14 +12,16 @@ const SuggestedUsers = () => {
     <VStack py={8} px={6} gap={4}>
       <SuggestedHeader />
 
-      <Flex alignItems={'center'} justifyContent={'space-between'} w={'full'}>
-        <Text fontSize={12} fontWeight={'bold'} color={'gray.500'}>
-          Suggested for you
-        </Text>
-        <Text fontSize={12} fontWeight={'bold'} _hover={{ color: 'gray.400' }} cursor={'pointer'}>
-          See All
-        </Text>
-      </Flex>
+      {suggestedUsers.length !== 0 && (
+        <Flex alignItems={'center'} justifyContent={'space-between'} w={'full'}>
+          <Text fontSize={12} fontWeight={'bold'} color={'gray.500'}>
+            Suggested for you
+          </Text>
+          <Text fontSize={12} fontWeight={'bold'} _hover={{ color: 'gray.400' }} cursor={'pointer'}>
+            See All
+          </Text>
+        </Flex>
+      )}
 
       {suggestedUsers.map((user) => (
         <SuggestedUser user={user} key={user.id} />
