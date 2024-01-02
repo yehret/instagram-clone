d6cfb2e0bd0a020bd84479dc543660d487734904
Serhii <sirozhka.v@gmail.com>
2024-01-02 17:12:35 +0100

    fixes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -19,3 +19,13 @@ const firestore = getFirestore(app);
 const storage = getStorage(app);
 
 export {app, auth, firestore, storage}
+
+// const firebaseConfig = {
+//       apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//       authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//       projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//       storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//       messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//       appId: import.meta.env.VITE_FIREBASE_APP_ID,
+//       measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+//     };
