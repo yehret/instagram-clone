4316aea1db9973c0202cd1ae402dd0f3fced9e7d
Serhii <sirozhka.v@gmail.com>
2024-01-02 16:54:38 +0100

    firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA7-ihfMxaeBqSBNWNwHu2dIp8J119iiOg",
-  authDomain: "insta-clone-e16b7.firebaseapp.com",
-  projectId: "insta-clone-e16b7",
-  storageBucket: "insta-clone-e16b7.appspot.com",
-  messagingSenderId: "603836844541",
-  appId: "1:603836844541:web:cae5019482be29590d7194",
-  measurementId: "G-118F31MYLC"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
